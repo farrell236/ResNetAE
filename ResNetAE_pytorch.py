158fa57aa4dd40b614a06d9f6c91867d00c8f0c8
Benjamin Hou <>
2020-06-26 16:42:25 +0100

    update pytorch code

--- ResNetAE_pytorch.py
+++ ResNetAE_pytorch.py
@@ -3,125 +3,220 @@ from __future__ import division
 from __future__ import print_function
 
 import torch
-import torch.nn as nn
 
 
-class ResNetAE(object):
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=(3, 3), stride=(1, 1)):
 
+        super(ResidualBlock, self).__init__()
+
+        self.residual_block = torch.nn.Sequential(
+            torch.nn.BatchNorm2d(in_channels),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                            kernel_size=kernel_size, stride=stride, padding=1),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
+                            kernel_size=kernel_size, stride=stride, padding=1)
+        )
+
+    def forward(self, x):
+        return x + self.residual_block(x)
+
+
+class ResNetAE_encoder(torch.nn.Module):
     def __init__(self,
                  n_ResidualBlock=8,
                  n_levels=4,
-                 z_dim=10,
-                 output_channels=1,
+                 input_ch=3,
+                 z_ch=10,
                  bUseMultiResSkips=True):
 
-        self.n_ResidualBlock = n_ResidualBlock
-        self.n_levels = n_levels
+        super(ResNetAE_encoder, self).__init__()
+
         self.max_filters = 2 ** (n_levels+3)
-        self.z_dim = z_dim
+        self.n_levels = n_levels
         self.bUseMultiResSkips = bUseMultiResSkips
-        self.output_channels = output_channels
 
-    def ResidualBlock(self, x, in_channels, out_channels=64, kernel_size=(3, 3), stride=(1, 1)):
-        """
-        Full pre-activation ResNet Residual block
-        https://arxiv.org/pdf/1603.05027.pdf
-        """
-        skip = x
-        x = nn.BatchNorm2d(in_channels)(x)
-        x = nn.ReLU()(x)
-        x = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                      kernel_size=kernel_size, stride=stride, padding=1)(x)
-        x = nn.BatchNorm2d(out_channels)(x)
-        x = nn.ReLU()(x)
-        x = nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
-                      kernel_size=kernel_size, stride=stride, padding=1)(x)
-        x = x + skip
-        return x
+        self.conv_list = []
+        self.res_blk_list = []
+        self.multi_res_skip_list = []
 
-    def encoder(self, x):
-        """
-        'Striving for simplicity: The all convolutional net'
-        arXiv: https://arxiv.org/pdf/1412.6806.pdf
-        'We find that max-pooling can simply be replaced by a convolutional layer
-        with increased stride without loss in accuracy on several image recognition benchmarks'
-        """
+        self.input_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=input_ch, out_channels=8,
+                            kernel_size=(3, 3), stride=(1, 1), padding=1),
+            torch.nn.BatchNorm2d(8),
+            torch.nn.ReLU(inplace=True),
+        )
 
-        x = nn.Conv2d(in_channels=3, out_channels=8,
-                      kernel_size=(3, 3), stride=(1, 1), padding=1)(x)
-        x = nn.ReLU()(x)
-
-        skips = []
-
-        for i in range(self.n_levels):
-
+        for i in range(n_levels):
             n_filters_1 = 2 ** (i + 3)
             n_filters_2 = 2 ** (i + 4)
-            ks = 2 ** (self.n_levels - i)
+            ks = 2 ** (n_levels - i)
 
+            self.res_blk_list.append(
+                torch.nn.Sequential(*[ResidualBlock(n_filters_1, n_filters_1) for _ in range(n_ResidualBlock)])
+            )
+
+            self.conv_list.append(
+                torch.nn.Sequential(
+                    torch.nn.Conv2d(n_filters_1, n_filters_2, kernel_size=(2, 2), stride=(2, 2), padding=0),
+                    torch.nn.BatchNorm2d(n_filters_2),
+                    torch.nn.ReLU(inplace=True),
+                )
+            )
+
+            if bUseMultiResSkips:
+                self.multi_res_skip_list.append(
+                    torch.nn.Sequential(
+                        torch.nn.Conv2d(in_channels=n_filters_1, out_channels=self.max_filters,
+                                        kernel_size=(ks, ks), stride=(ks, ks), padding=0),
+                        torch.nn.BatchNorm2d(self.max_filters),
+                        torch.nn.ReLU(inplace=True),
+                    )
+                )
+
+        self.output_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=self.max_filters, out_channels=z_ch,
+                            kernel_size=(3, 3), stride=(1, 1), padding=1),
+            torch.nn.BatchNorm2d(z_ch),
+            torch.nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+
+        x = self.input_conv(x)
+
+        skips = []
+        for i in range(self.n_levels):
+            x = self.res_blk_list[i](x)
             if self.bUseMultiResSkips:
-                _x = nn.Conv2d(in_channels=n_filters_1, out_channels=self.max_filters,
-                               kernel_size=(ks, ks), stride=(ks, ks))(x)
-                _x = nn.ReLU()(_x)
-                skips.append(_x)
-
-            for _ in range(self.n_ResidualBlock):
-                x = self.ResidualBlock(x, in_channels=n_filters_1, out_channels=n_filters_1)
-
-            x = nn.Conv2d(in_channels=n_filters_1, out_channels=n_filters_2,
-                          kernel_size=(2, 2), stride=(2, 2), padding=0)(x)
-            x = nn.ReLU()(x)
+                skips.append(self.multi_res_skip_list[i](x))
+            x = self.conv_list[i](x)
 
         if self.bUseMultiResSkips:
             x = sum([x] + skips)
 
-        x = nn.Conv2d(in_channels=n_filters_2, out_channels=self.z_dim,
-                      kernel_size=(3, 3), stride=(1, 1), padding=1)(x)
-        x = nn.ReLU()(x)
+        x = self.output_conv(x)
 
         return x
 
-    def decoder(self, z):
 
-        z = z_top = nn.ReLU()(
-            nn.Conv2d(in_channels=self.z_dim, out_channels=self.max_filters,
-                      kernel_size=(3, 3), stride=(1, 1), padding=1)(z))
+class ResNetAE_decoder(torch.nn.Module):
+    def __init__(self,
+                 n_ResidualBlock=8,
+                 n_levels=4,
+                 z_ch=10,
+                 output_ch=3,
+                 bUseMultiResSkips=True):
 
-        for i in range(self.n_levels):
+        super(ResNetAE_decoder, self).__init__()
 
+        self.max_filters = 2 ** (n_levels+3)
+        self.n_levels = n_levels
+        self.bUseMultiResSkips = bUseMultiResSkips
+
+        self.conv_list = []
+        self.res_blk_list = []
+        self.multi_res_skip_list = []
+
+        self.input_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=z_ch, out_channels=self.max_filters,
+                            kernel_size=(3, 3), stride=(1, 1), padding=1),
+            torch.nn.BatchNorm2d(self.max_filters),
+            torch.nn.ReLU(inplace=True),
+        )
+
+        for i in range(n_levels):
             n_filters_0 = 2 ** (self.n_levels - i + 3)
             n_filters_1 = 2 ** (self.n_levels - i + 2)
-            ks = 2 ** (i+1)
+            ks = 2 ** (i + 1)
 
-            z = nn.ConvTranspose2d(in_channels=n_filters_0, out_channels=n_filters_1,
-                                   kernel_size=(2, 2), stride=(2, 2))(z)
-            z = nn.ReLU()(z)
+            self.res_blk_list.append(
+                torch.nn.Sequential(*[ResidualBlock(n_filters_1, n_filters_1) for _ in range(n_ResidualBlock)])
+            )
 
-            for _ in range(self.n_ResidualBlock):
-                z = self.ResidualBlock(z, in_channels=n_filters_1, out_channels=n_filters_1)
+            self.conv_list.append(
+                torch.nn.Sequential(
+                    torch.nn.ConvTranspose2d(n_filters_0, n_filters_1,
+                                             kernel_size=(2, 2), stride=(2, 2), padding=0),
+                    torch.nn.BatchNorm2d(n_filters_1),
+                    torch.nn.ReLU(inplace=True),
+                )
+            )
 
+            if bUseMultiResSkips:
+                self.multi_res_skip_list.append(
+                    torch.nn.Sequential(
+                        torch.nn.ConvTranspose2d(in_channels=self.max_filters, out_channels=n_filters_1,
+                                                 kernel_size=(ks, ks), stride=(ks, ks), padding=0),
+                        torch.nn.BatchNorm2d(n_filters_1),
+                        torch.nn.ReLU(inplace=True),
+                    )
+                )
+
+        self.output_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=n_filters_1, out_channels=output_ch,
+                            kernel_size=(3, 3), stride=(1, 1), padding=1),
+            torch.nn.BatchNorm2d(output_ch),
+            torch.nn.ReLU(inplace=True),
+        )
+
+    def forward(self, z):
+
+        z = z_top = self.input_conv(z)
+
+        for i in range(self.n_levels):
+            z = self.conv_list[i](z)
+            z = self.res_blk_list[i](z)
             if self.bUseMultiResSkips:
-                _z = nn.ConvTranspose2d(in_channels=self.max_filters, out_channels=n_filters_1,
-                                        kernel_size=(ks, ks), stride=(ks, ks))(z_top)
-                _z = nn.ReLU()(_z)
-                z = z + _z
+                z += self.multi_res_skip_list[i](z_top)
 
-        z = nn.Conv2d(in_channels=n_filters_1, out_channels=self.output_channels,
-                      kernel_size=(3, 3), stride=(1, 1), padding=1)(z)
-        z = nn.ReLU()(z)
+        z = self.output_conv(z)
 
         return z
 
 
+class ResNetVAE(torch.nn.Module):
+    def __init__(self):
+        super(ResNetVAE, self).__init__()
+
+        self.encoder = ResNetAE_encoder()
+        self.decoder = ResNetAE_decoder()
+
+        # Assumes the input to be of shape 256x256
+        z_dim = 20
+        self.fc21 = torch.nn.Linear(2560, z_dim)
+        self.fc22 = torch.nn.Linear(2560, z_dim)
+        self.fc3 = torch.nn.Linear(z_dim, 2560)
+
+    def encode(self, x):
+        h1 = self.encoder(x) # h1 should be batch 10*16*16
+        return self.fc21(h1.view(-1, 2560)), self.fc22(h1.view(-1, 2560))
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = self.decoder(self.fc3(z).view(-1, 10, 16, 16))
+        return torch.sigmoid(h3)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
 if __name__ == '__main__':
 
-    model = ResNetAE(n_ResidualBlock=8,
-                     n_levels=4,
-                     z_dim=10,
-                     output_channels=3,
-                     bUseMultiResSkips=True)
+    encoder = ResNetAE_encoder()
+    decoder = ResNetAE_decoder()
 
     test_input = torch.rand(10, 3, 256, 256)
+    out = encoder(test_input)
 
-    z = model.encoder(test_input)
-    out = model.decoder(z)
+    test_input = torch.rand(10, 10, 16, 16)
+    out = decoder(test_input)
